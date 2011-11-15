b1568a148f750d10f9820863049a4afa7623b858
Aron Lakatos <aron.lakatos@gmail.com>
2011-11-15 16:33:12 +0000

    Merged H5BP with my own project templates

--- js/script.js
+++ js/script.js
@@ -1,8 +1,32 @@
-/* Author:
+/* Author:	Aron Lakatos / @aronlakatos */
 
-*/
+function initPNGfix(imgList) {
+	if ( $('html').hasClass('msie6') ) {		// execute in ie6 only (relies on jQuery Browser Plugin 2.3)
+		for (x in imgList) {
+			if ( imgList[x].length ) {
+				DD_belatedPNG.fix(imgList[x]);
+			};
+			x++;
+		}		
+	}
+}
 
+function template(s) {
+	return false;
+}
 
+$(document).ready(function() {
+	
+    var PNGimageList = [
+		'.trans01SAMPLE',
+		'.trans02SAMPLE'
+	];
+    initPNGfix(PNGimageList);
 
+    var bgiframeList = [
+		'.overlaySAMPLE'
+	];
+    initBgiframe(bgiframeList);
 
-
+	template();	
+});
